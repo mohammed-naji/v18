a38d01d255fffc54ae37d128f33d61a5ba909380
Mohammed Naji <m.naji1512@gmail.com>
2023-08-01 12:52:55 +0300

    lec4 - route

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,14 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// use , namespace
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return 'homepage';
 });
 
-Route::get('/about', function () {
-    return view('welcome');
-});
-
-Route::get('/contact', function () {
-    return view('welcome');
-});
-
-Route::get('/contact', function () {
-    return view('welcome');
+Route::get('/contact-us', function() {
+    return 'contact us';
 });
